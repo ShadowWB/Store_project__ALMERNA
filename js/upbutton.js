d69f390cb0444d23afcb9188e96d49b3037454d1
ShadowWB <f.kastro.ua@gmail.com>
2022-11-21 22:29:34 +0200

    lone

---
+++ js/upbutton.js
@@ -0,0 +1,13 @@
+const upButton = document.querySelector(".up__button");
+
+upButton.addEventListener('click',()=>{
+    document.documentElement.scrollTop = 0;
+    console.log('dssds')
+})
+window.onscroll = function (){
+    if (document.documentElement.scrollTop>20){
+        upButton.style.display= "block";
+    }else{
+        upButton.style.display= "none";
+    }
+}
